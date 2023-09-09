8517bfb7254d8c7707ef56adc1242c1b406260a4
Sepideh <ssmazlumi@gmail.com>
2023-09-09 21:08:45 +0330

    Edit Route

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,5 @@
 import { useRouter } from 'next/router'
 import '../styles/globals.css'
-import { motion, AnimatePresence } from 'framer-motion'
-import CustomCoursor from '../src/components/common/customCursor/customCoursor';
 import Footer from '../src/components/Footer/Footer';
 
 
@@ -10,7 +8,7 @@ function MyApp({ Component, pageProps })
   const router = useRouter();
   return (
     <>
-      < Component {...pageProps} />
+      <Component {...pageProps} />
       <Footer />
     </>
   )
